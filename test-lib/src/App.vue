c80eef67fbcab4f412af39619d9a2e61919eadda
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-10-17 19:30:48 +0200

    add test-lib

---
+++ test-lib/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <SampleComponent headingText="123" bodyText="122333" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { SampleComponent } from "webcie-vcl";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    SampleComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
