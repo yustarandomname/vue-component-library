dba853c97ceaf03277be324d817d0ff309157707
Abel de Bruijn <A.deBruijn-3@student.tudelft.nl>
2021-10-20 10:52:31 +0200

    import all components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import SampleComponent from "./SampleComponent/SampleComponent.vue";
-import Avatar from "./Avatar/Avatar.vue";
 
-export { SampleComponent, Avatar };
+// Components
+import Avatar from "./Avatar/Avatar.vue";
+import Button from "./Button/Button.vue";
+import Grid from "./Grid/Grid.vue";
+import Icon from "./Icon/Icon.vue";
+import InfiniteList from "./InfiniteList/InfiniteList.vue";
+import Modal from "./Modal/Modal.vue";
+import NavigationBar from "./NavigationBar/NavigationBar.vue";
+
+export { Avatar, Button, Grid, Icon, InfiniteList, Modal, NavigationBar, SampleComponent };
